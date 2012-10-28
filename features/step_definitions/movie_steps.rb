59e4605365d367313888bbb83ded76669bd4caf4
saasbook <saasbook@saasbook.(none)>
2012-10-28 11:03:56 -0700

    modified:   features/sort_movie_list.feature 	modified:   features/step_definitions/movie_steps.rb

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -22,7 +22,11 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.content  is the entire content of the page as a string.
-  flunk "Unimplemented"
+
+  match = /#{e1}.*#{e2}/m =~ page.body
+
+  assert match, "#{e2} was found after #{e1}"
+
 end
 
 # Make sure we only have movies with the specified ratings displayed in the table
